df28080366b9a136b8cc14bf524adb4a45c5bd73
Nicolás Omar González Passerino <nicolas.passerino@gmail.com>
2020-05-24 18:43:53 -0300

    Fourth assignment completed

--- 1-learning/src/app/app/assignments/fourth/fourth.component.ts
+++ 1-learning/src/app/app/assignments/fourth/fourth.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fourth.component.css']
 })
 export class FourthComponent implements OnInit {
+  numbers: number[] = [];
+  ref;
+
+  toggleGame(runGame: boolean) {
+    if (runGame) {
+      this.ref = setInterval(() => this.numbers.push(this.numbers.length + 1), 1000)
+    } else {
+      clearInterval(this.ref)
+    }
+  }
 
   constructor() { }
 
--- 1-learning/src/app/app/assignments/fourth/game-control/game-control.component.ts
+++ 1-learning/src/app/app/assignments/fourth/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() startGame = new EventEmitter<boolean>();
+  @Output() stopGame = new EventEmitter<boolean>();
+
+  startingGame = () => this.stopGame.emit(true)
+
+  stoppingGame = () => this.stopGame.emit(false)
 
   constructor() { }
 
